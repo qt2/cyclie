317e3702aa74ad618455eee3d8d39138923bc80d
qrtz <81661096+qt2@users.noreply.github.com>
2024-02-26 12:11:57 +0900

    Add dataset

--- server-recommend/main.py
+++ server-recommend/main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return {"version": "v1.0.0"}
+
+
+@app.get("/search")
+async def search(
+    p_rent: int = None,
+    rent: int = None,
+    p_area: int = None,
+    area: int = None,
+    p_distance: int = None,
+    distance: int = None,
+    p_age: int = None,
+    age: int = None,
+    p_atmos: int = None,
+    a_modern: int = None,
+):
+    pass
